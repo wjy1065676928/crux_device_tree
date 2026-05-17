88a5e1cc3b8de06b10bdf9d2c8e5127253f452b1
wjy1065676928 <wjy1065676928@gmail.com>
2026-05-17 12:57:41 +0800

    在 twrp_crux.mk 中添加 PRODUCT_RELEASE_NAME 变量

--- twrp_crux.mk
+++ twrp_crux.mk
@@ -5,6 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+PRODUCT_RELEASE_NAME := crux
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
